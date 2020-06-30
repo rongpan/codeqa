b470d0883157b8a16d1a01a8621a6c24c2b21f4b
rongpan <rongpan17@gmail.com>
2020-06-30 16:58:12 -0500

    init

---
+++ src/test/java/com/codeqa/forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeqa.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
