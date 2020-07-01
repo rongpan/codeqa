646eb0d48315b5da1efc805134f6373a60b4e780
rongpan <rongpan17@gmail.com>
2020-07-01 15:12:37 -0500

    github user

--- src/main/java/com/codeqa/forum/controller/AuthorizeController.java
+++ src/main/java/com/codeqa/forum/controller/AuthorizeController.java
@@ -1,14 +1,31 @@
 package com.codeqa.forum.controller;
 
+import com.codeqa.forum.dto.AccessTokenDTO;
+import com.codeqa.forum.dto.GithubUser;
+import com.codeqa.forum.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("Iv1.6fcf47f2a229f736");
+        accessTokenDTO.setClient_secret("1ffb5b5fc45f977f4a399c4ecbdfc785311ba1ec");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        System.out.println(githubUser.getName());
         return "index";
     }
 }
