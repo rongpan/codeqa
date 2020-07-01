6e0e2824270e85ee5dd9ad3a51f01f511af78534
rongpan <rongpan17@gmail.com>
2020-07-01 15:55:08 -0500

    use properties

--- src/main/java/com/codeqa/forum/controller/AuthorizeController.java
+++ src/main/java/com/codeqa/forum/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.codeqa.forum.dto.AccessTokenDTO;
 import com.codeqa.forum.dto.GithubUser;
 import com.codeqa.forum.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,14 +15,23 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("Iv1.6fcf47f2a229f736");
-        accessTokenDTO.setClient_secret("1ffb5b5fc45f977f4a399c4ecbdfc785311ba1ec");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = githubProvider.getUser(accessToken);
