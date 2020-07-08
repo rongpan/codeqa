9f8dd0408b7bfb64f2fc2bd64a6bec8a657d7aea
rongpan <rongpan17@gmail.com>
2020-07-08 15:57:19 -0500

    profile

--- src/main/java/com/codeqa/forum/controller/IndexController.java
+++ src/main/java/com/codeqa/forum/controller/IndexController.java
@@ -1,8 +1,6 @@
 package com.codeqa.forum.controller;
 
 import com.codeqa.forum.dto.PaginationDTO;
-import com.codeqa.forum.mapper.UserMapper;
-import com.codeqa.forum.model.User;
 import com.codeqa.forum.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,34 +8,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
 public class IndexController {
-
-    @Autowired
-    private UserMapper userMapper;
 
     @Autowired
     private QuestionService questionService;
 
     @GetMapping("/")
-    public String index(HttpServletRequest request, Model model,
-                        @RequestParam(name = "page", defaultValue = "1") Integer page,
+    public String index(Model model, @RequestParam(name = "page", defaultValue = "1") Integer page,
                         @RequestParam(name = "size", defaultValue = "5") Integer size) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null && cookies.length != 0)
-            for(Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    User user = userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
         PaginationDTO pagination = questionService.list(page, size);
         model.addAttribute("pagination", pagination);
         return "index";
---
+++ src/main/java/com/codeqa/forum/controller/ProfileController.java
@@ -0,0 +1,43 @@
+package com.codeqa.forum.controller;
+
+import com.codeqa.forum.dto.PaginationDTO;
+import com.codeqa.forum.mapper.UserMapper;
+import com.codeqa.forum.model.User;
+import com.codeqa.forum.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ProfileController {
+    @Autowired
+    private QuestionService questionService;
+
+    @GetMapping("/profile/{action}")
+    public String profile(HttpServletRequest request, @PathVariable(name = "action") String action,
+                          Model model, @RequestParam(name = "page", defaultValue = "1") Integer page,
+                          @RequestParam(name = "size", defaultValue = "5") Integer size) {
+
+        User user = (User) request.getSession().getAttribute("user");
+        if (user == null) {
+            return "redirect:/";
+        }
+
+        if ("questions".equals(action)) {
+            model.addAttribute("section", "questions");
+            model.addAttribute("sectionName", "My posts");
+        } else if ("replies".equals(action)) {
+            model.addAttribute("section", "replies");
+            model.addAttribute("sectionName", "Latest replies");
+        }
+        PaginationDTO paginationDTO = questionService.list(user.getId(), page, size);
+        model.addAttribute("pagination", paginationDTO);
+        return "profile";
+    }
+}
