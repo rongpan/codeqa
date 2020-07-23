60da434d8b9cec3ed67458dedfd7ce5293a323bb
rongpan <rongpan17@gmail.com>
2020-07-23 12:24:12 -0500

    log in when replying

--- src/main/resources/static/js/forum.js
+++ src/main/resources/static/js/forum.js
@@ -14,9 +14,16 @@ function post() {
             if (response.code == 200) {
                 $("#comment_section").hide();
             } else {
-                alert(response.message);
+                if (response.code == 2003) {
+                    var isAccepted = confirm(response.message);
+                    if (isAccepted) {
+                        window.open("https://github.com/login/oauth/authorize?client_id=Iv1.6fcf47f2a229f736&redirect_uri=http://localhost:5000/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable", true);
+                    }
+                } else {
+                    alert(response.message);
+                }
             }
-            console.log(response);
         },
         dataType: "json"
     });
